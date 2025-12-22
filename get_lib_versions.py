d2ebef3a9260d8eb62281770ec59e50b17d4ce5b
Sainadh-Bahadursha <107560972+Sainadh-Bahadursha@users.noreply.github.com>
2025-12-22 21:44:41 +0530

    Worked Agentic Rag with mcp server and deployment - need to test

--- get_lib_versions.py
+++ get_lib_versions.py
@@ -1,11 +1,30 @@
 import importlib.metadata
 packages = [
+    "beautifulsoup4",
+    "fastapi",
+    "html5lib",
+    "jinja2",
     "langchain",
-    "langchain-core",
+    "langchain-astradb",
+    "langchain_core",
+    "langchain-google-genai",
+    "langchain-groq",
+    "lxml",
     "python-dotenv",
+    "python-multipart",
+    "selenium",
     "streamlit",
-    "ipykernel"
+    "undetected-chromedriver",
+    "uvicorn",
+    "structlog",
+    "langgraph",
+    "ragas",
+    "langchain-mcp-adapters",
+    "mcp",
+    "ddgs",
+    "langchain-openai"
 ]
+
 for pkg in packages:
     try:
         version = importlib.metadata.version(pkg)
